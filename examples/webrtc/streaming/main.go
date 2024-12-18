ba67847f32bc8fd83e2e5576dceb170939e5985b
thanhlong1203 <longma350@gmail.com>
2024-12-18 17:24:30 +0700

    add send audio track

--- examples/webrtc/streaming/main.go
+++ examples/webrtc/streaming/main.go
@@ -16,8 +16,8 @@ func main() {
 
 func Streaming() {
 
-	clanId := "xxx"
-	channelId := "xxx"
+	clanId := "1775732550744936448"    // KOMU_2
+	channelId := "1837040466697129984" // NCC8
 	wsConn, err := radiostation.NewWSConnection(&radiostation.Config{
 		BasePath:     "stn.nccsoft.vn",
 		Timeout:      10,
@@ -28,11 +28,12 @@ func Streaming() {
 		panic(err)
 	}
 
-	filePath := "test.mp3"
+	// ffmpeg -i test.mp3 -c:a libopus -page_duration 20000 test.ogg;
+	filePath := "ncc8.ogg"
 	rtcConn, err := mezonsdk.NewStreamingRTCConnection(webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
 			{
-				URLs: []string{"stun.l.google.com:19302"}, // TODO: check radio station ice server
+				URLs: []string{"stun:stun.l.google.com:19302"}, // TODO: check radio station ice server
 			},
 		},
 	}, wsConn, clanId, channelId)
@@ -40,7 +41,7 @@ func Streaming() {
 		panic(err)
 	}
 
-	err = rtcConn.SendFile(filePath)
+	err = rtcConn.SendAudioTrack(filePath)
 	if err != nil {
 		panic(err)
 	}
